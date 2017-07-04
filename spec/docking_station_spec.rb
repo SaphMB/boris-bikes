7809ff5f1ec678cff55f79902f742281a40a5a7c
artemis <artemisxen@gmail.com>
2017-07-04 16:55:11 +0100

    writes working test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -2,11 +2,17 @@ require 'docking_station'
 
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
-end
 
-describe DockingStation do
   it "Creates new bike" do
     docking_station = DockingStation.new
-    expect(docking_station.release_bike).to eq(Bike.new)
+    expect(docking_station.release_bike).to be_instance_of Bike
   end
+
+  it "bike is_working?" do
+    docking_station = DockingStation.new
+    expect(docking_station.release_bike.working?).to eq true
+  end
+
+
+
 end
