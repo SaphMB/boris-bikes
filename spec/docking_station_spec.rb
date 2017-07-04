7fc4f8826fc353de7e676097b7164811fb8b7736
Sapphire Mason-Brown <saphmb@gmail.com>
2017-07-04 16:33:31 +0100

    Write release_bike test

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,3 +3,10 @@ require 'docking_station'
 describe DockingStation do
   it { is_expected.to respond_to :release_bike }
 end
+
+describe DockingStation do
+  it "Creates new bike" do
+    docking_station = DockingStation.new
+    expect(docking_station.release_bike).to eq(Bike.new)
+  end
+end
